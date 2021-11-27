bac172fd8a5947f20442699ded6c73b27f5e95fe
Sodinapalli-Karthik <sodinapallikarthik@gmail.com>
2021-11-28 00:28:57 +0530

    signin signup landingpages added

---
+++ spotify-clone-frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Landingpage from './Mycomponents/Landingpage.js';
+
+function App() {
+  return (
+    <>
+      <Landingpage></Landingpage>
+    </>
+
+
+
+
+
+
+
+
+  );
+}
+
+export default App;
